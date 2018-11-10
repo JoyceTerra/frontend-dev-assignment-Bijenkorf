77c9e3712b77929e95d3f8163f3794247e585e8b
JoyceTerra <joyterra@gmail.com>
2018-11-10 15:17:06 +0100

    Clear input user and active state implemented to InputForm component

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 import  './InputForm.css'
 
 class InputForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      search: ''
+    }
+  }
+
+  setActive = () => {
+    this.setState({active: !this.state.active})
+  }
+
+  handleChange = (e) => {
+    this.setState({search: e.target.value})
+  }
+
+  resetSearch = () => {
+    this.setState({search: ''})
+  }
 
     render() {
       return (
-        <div className="input-container">
-            <form>
-                <input className="input-field" type="text" aria-label="input search" placeholder={this.props.placeholder} /> 
-            </form>
-          </div>
-
+        <div>
+          <form>
+            <input className="input-field" 
+                        type="text" 
+                        name="input" 
+                        aria-label="input search" 
+                        value={this.state.search} 
+                        onFocus={this.setActive} 
+                        onChange={(e) => this.handleChange(e)} 
+                        placeholder={this.props.placeholder} 
+              /> {console.log(this.state)}
+            <button className="clear-input" type="reset" onClick={this.resetSearch} ></button>
+          </form>
+        </div>
       )
     }
   }
   
-  export default InputForm;
+  export default InputForm
 
 
